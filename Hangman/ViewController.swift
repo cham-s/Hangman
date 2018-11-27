d1d1e73088db0d82a39689860b87187ea71cbe64
cham-s <cc.attou@gmail.com>
2018-11-27 16:23:51 +0100

    Initial Commit

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by chams on 27/11/2018.
+//  Copyright © 2018 Chams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
