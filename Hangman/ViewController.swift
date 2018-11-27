b9b817a3858e97eef4b7ef3ff305d2e2c007bb42
cham-s <cc.attou@gmail.com>
2018-11-27 17:46:59 +0100

    added loadwords setNewWord

--- Hangman/ViewController.swift
+++ Hangman/ViewController.swift
@@ -19,13 +19,35 @@ class ViewController: UIViewController {
         }
     }
     
+    var words = [String]()
+    var usedLetter = [Character]()
+    var currentWord = [Character]()
+    var usedWord = [String]()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadWordsDefault()
+        setNewWord()
     }
-
-
+    
+    func setNewWord() {
+        let index = Int.random(in: 0..<words.count)
+        currentWord = Array(words.remove(at: index))
+        let blanks = String(repeating: "_ ", count: currentWord.count)
+        wordLabel.text = blanks
+    }
+    
+    func loadWordsDefault() {
+        if let wordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            if let contents = try? String(contentsOfFile: wordsPath) {
+                words = contents.components(separatedBy: "\n")
+            } else {
+                words = ["hello", "world"]
+            }
+        } else {
+            words = ["hello", "world"]
+        }
+        words.shuffle()
+    }
 }
 
